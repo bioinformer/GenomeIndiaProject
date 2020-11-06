fa2eb2d215bd8ccfbe0586258cbca1ece4f48a71
Praharshit Sharma <praharshit.sharma@gmail.com>
2020-11-06 13:17:20 +0530

    Update gLocal_Sliding-Window.sh

--- gLocal_Sliding-Window.sh
+++ gLocal_Sliding-Window.sh
@@ -13,11 +13,11 @@ mv GCF_000001405.39_GRCh38.p13_genomic.fna grch38.fa
 
 # Convert both into Single-line FASTA formats,
 grep -v ">" hg19.fa | sed -e 's/\n//g' -e 's/ //g' | tee hg19seq.fa
-## $ cat> hg19hdr.fa
-## >hg19
-## ^C
-## cat hg19hdr.fa hg19seq.fa >> hg19new.fa
-## awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' hg19new.fa | tee hg19sng.fa
+$ cat> hg19hdr.fa
+>hg19
+^C
+cat hg19hdr.fa hg19seq.fa >> hg19new.fa
+awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' hg19new.fa | tee hg19sng.fa
 
 # Compute the 'PRECISE' Sliding Window scaling, based on Differential Genome-Lengths,
 # 3,272,089,205 / 3,137,161,264 = 1.0430095649
